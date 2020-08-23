3fc84bea8fa0ec49664ed2d48a331b7c8982c5a4
Igor <id.anpilogov@gmail.com>
2020-08-23 18:42:13 +0400

    Autogenerated task ltx files, task ltx generator

---
+++ taskmaker/db.py
@@ -0,0 +1,71 @@
+locations = {
+    "agroprom": {
+        "agr_smart_terrain_1_6_army_mechanic_stalker": "MECHANIC",
+        "agr_1_6_medic_army_mlr": "MEDIC",
+    },
+    "bar": {
+        "bar_visitors_stalker_mechanic": "MECHANIC",
+        "bar_dolg_medic": "MEDIC",
+    },
+    "darkscape": {
+        "dasc_tech_mlr": "MECHANIC",
+    },
+    "darkvalley": {
+        "val_smart_terrain_7_3_bandit_mechanic_stalker": "MECHANIC",
+        "bandit_main_base_medic_mlr": "MEDIC",
+    },
+    "deadcity": {
+        "cit_killers_merc_mechanic_stalker": "MECHANIC",
+        "cit_killers_merc_medic_stalker": "MEDIC",
+    },
+    "escape": {
+        "esc_smart_terrain_5_7_loner_mechanic_stalker": "MECHANIC",
+        "army_south_mechan_mlr": "MECHANIC",
+    },
+    "jupiter": {
+        "jup_b217_stalker_tech": "MECHANIC",
+        "jup_cont_mech_bandit": "MECHANIC",
+        "mechanic_monolith_jup_depo": "MECHANIC",
+        "jup_a6_stalker_medik": "MEDIC",
+    },
+    "marsh": {
+        "mar_base_stalker_tech": "MECHANIC",
+        "mar_smart_terrain_base_doctor": "MEDIC",
+    },
+    "military": {
+        "mil_smart_terrain_7_7_freedom_mechanic_stalker": "MECHANIC",
+        "mil_freedom_medic": "MEDIC",
+    },
+    "pripyat2": {
+        "pri_monolith_monolith_mechanic_stalker": "MECHANIC",
+    },
+    "red_forest": {
+        "red_greh_tech": "MECHANIC",
+    },
+    "truck": {
+        "trucks_cemetery_bandit_mechanic": "MECHANIC",
+    },
+    "yantar": {
+        "mechanic_army_yan_mlr": "MECHANIC",
+    },
+    "zaton": {
+        "zat_a2_stalker_mechanic": "MECHANIC",
+        "zat_stancia_mech_merc": "MECHANIC",
+        "zat_tech_mlr": "MECHANIC",
+        "zat_b22_stalker_medic": "MEDIC",
+    },
+}
+
+quests = {
+    "MECHANIC": {
+        "transaction": "ui_inGame2_Sdelka",
+        "supply": "ui_inGame2_Osobiy_zakaz",
+        "weapon": "ui_inGame2_Neizvestnoe_oruzhie",
+        "mutant": "ui_iconsTotal_mutant",
+        "heavy": "ui_inGame2_PD_master_boevih_sistem",
+    },
+    "MEDIC": {
+        "artifact_world": "ui_inGame2_Kontrakt_s_uchenimi",
+        "artifact_stalker": "ui_inGame2_Kontrakt_s_uchenimi",
+    },
+}
---
+++ taskmaker/main.py
@@ -0,0 +1,57 @@
+import db
+
+op = "{"
+cl = "}"
+COUNT_START = 10
+TEXT_HEADER = "gt_task_text_"
+STAGES = 2
+SPACE_COUNT = 21
+FILE_HEADER = """;============================================================
+;	Anomaly 1.5.5.0
+;	Igigog
+;============================================================
+"""
+
+
+def main():
+    for location, npcs in db.locations.items():
+        with open(f"task/gt_task_{location}.ltx", 'w') as f:
+            f.write(FILE_HEADER)
+            for npc, typ in npcs.items():
+                count = COUNT_START
+                tasks = db.quests[typ]
+                f.write(f"""
+;============================================================
+;{" "*(29-len(typ)//2)}{typ} 
+;{" "*(29-len(npc)//2)}{npc}
+;============================================================""")
+                for task_name, icon in tasks.items():
+                    task_id = f"{npc}_task_{count}"
+                    f.write(f"""
+[{task_id}]
+icon {" "*(SPACE_COUNT-len("icon"))}= {icon}
+storyline {" "*(SPACE_COUNT-len("storyline"))}= false
+prior {" "*(SPACE_COUNT-len("prior"))}= 2000
+repeat_timeout {" "*(SPACE_COUNT-len("repeat_timeout"))}= 16200
+precondition {" "*(SPACE_COUNT-len("precondition"))}= {op + f"=validate_generic_task({task_id}:{task_name})" + cl} true, false
+
+title_functor {" "*(SPACE_COUNT-len("title_functor"))}= igi_task_generic_text
+descr_functor {" "*(SPACE_COUNT-len("descr_functor"))}= igi_task_generic_text
+job_descr {" "*(SPACE_COUNT-len("job_descr"))}= {TEXT_HEADER}{task_name}_job_descr
+task_complete_descr {" "*(SPACE_COUNT-len("task_complete_descr"))}= {TEXT_HEADER}_{task_name}_finish
+                    
+stage_complete {" "*(SPACE_COUNT-len("stage_complete"))}= {STAGES}
+status_functor {" "*(SPACE_COUNT-len("status_functor"))}= igi_task_generic_status
+target_functor {" "*(SPACE_COUNT-len("target_functor"))}= igi_task_generic_target
+on_job_descr {" "*(SPACE_COUNT-len("on_job_descr"))}= %=igi_task_generic_setup({task_id})%
+
+on_complete {" "*(SPACE_COUNT-len("on_complete"))}= %=igi_task_generic_finish({task_id}:true)%
+on_fail {" "*(SPACE_COUNT-len("on_fail"))}= %=igi_task_generic_finish({task_id}:false))%
+condlist_0 {" "*(SPACE_COUNT-len("condlist_0"))}= {op + f"!task_giver_alive({task_id})" + cl} fail
+;------------------------------------------------
+                    """)
+                    count += 1
+
+
+if __name__ == '__main__':
+    main()
