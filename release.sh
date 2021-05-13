76eab06d4b0feec4573f190a46cb2fd596d2c392
Igigog <Igigog21@gmail.com>
2021-05-13 18:28:52 +0200

    ADD: script for fast release

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+VERSION=$(grep '^TASKS_VERSION =' gamedata/scripts/igi_generic_task.script | sed 's/TASKS_VERSION = "\(.*\)".*/\1/')
+LASTCOMMIT=$(git log --oneline | head -n 1 | sed 's/\(.......\).*/\1/')
+git tag $VERSION $LASTCOMMIT 
+git push origin $VERSION
+
+echo "Authorization: token $GITHUB_TOKEN"
+curl \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Content-Type:application/json" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    https://api.github.com/repos/Igigog/Igi_Tasks/releases \
+    -d "{\"tag_name\":\"$VERSION\"}" || exit
+
+NAME="Igi_Tasks_$VERSION"
+NAME_FULL="${NAME}_DEV"
+cd .. && zip -r -q "Igi_Tasks/$NAME_FULL.zip" Igi_Tasks && cd -
+
+mkdir ./"$NAME"
+cp -r gamedata ./"$NAME"/gamedata
+zip -r -q "$NAME.zip" "$NAME"
+rm -r ./"$NAME"
+
+./upload_asset.sh owner=Igigog repo=Igi_Tasks tag=$VERSION filename="$NAME.zip"
+./upload_asset.sh owner=Igigog repo=Igi_Tasks tag=$VERSION filename="$NAME_FULL.zip"
+
+rm ./"$NAME.zip"
+rm ./"$NAME_FULL.zip"
+
+echo DONE!
