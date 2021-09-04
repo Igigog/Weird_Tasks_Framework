5d1cdcc551b0f3a7179c7204567e3c284d37278f
Igor Anpilogov <id.anpilogov@gmail.com>
2021-09-04 15:51:17 +0200

    UPD: Igi_Tasks -> WTF

--- test_framework.bat
+++ test_framework.bat
@@ -1,3 +1,3 @@
-@RD /S /Q "D:\Games\Anomaly\MO2\mods\Igi_Tasks"
-XCOPY /e /i /q "D:\Tasks\Igi_Tasks" "D:\Games\Anomaly\MO2\mods\Igi_Tasks"
+@RD /S /Q "D:\Games\Anomaly\MO2\mods\Weird_Tasks_Framework"
+XCOPY /e /i /q "D:\Tasks\Weird_Tasks_Framework" "D:\Games\Anomaly\MO2\mods\Weird_Tasks_Framework"
 START "" "D:\Desktop\MO2Anomaly.lnk"
